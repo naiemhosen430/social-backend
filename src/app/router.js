f2082fb047babfc4e4109a11e60924f863a2439d
naiemhosen430 <naiemhosen430@gmail.com>
2024-10-12 01:59:49 +0600

    d

--- src/app/router.js
+++ src/app/router.js
@@ -1,24 +1,16 @@
 import express from "express";
 import userRouter from "../modules/user/user.router.js";
-import postRouter from "../modules/post/post.router.js";
-import chatRouter from "../modules/chat/chat.router.js";
-import searchRouter from "../modules/search/search.router.js";
-import ntfRouter from "../modules/Notification/ntf.Router.js";
 import authRouter from "../modules/auth/auth.router.js";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  console.log("diahoo serber is runing");
+  console.log("Social inventory serber is runing");
 
-  res.json("Diahoo server is runing");
+  res.json("Social inventory server is runing");
 });
 
 // user router
 router.use("/api/v3/auth", authRouter);
 router.use("/api/v3/user", userRouter);
-router.use("/api/v3/post", postRouter);
-router.use("/api/v3/chat", chatRouter);
-router.use("/api/v3/search", searchRouter);
-router.use("/api/v3/ntf", ntfRouter);
 
 export default router;
